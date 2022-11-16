79983a911c733495baa261b8e8cac4ef31b377db
John Harvey Agrabio <76051633+agrabio134@users.noreply.github.com>
2022-11-16 12:02:58 +0800

    Add files via upload

---
+++ api/employee-system/header.php
@@ -0,0 +1,12 @@
+	
+	<meta charset = "utf-8" />
+		<link rel = "stylesheet" type = "text/css" href = "assets/css/bootstrap.min.css" />
+		<link rel = "stylesheet" type = "text/css" href = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+		<link rel = "stylesheet" type = "text/css" href = "assets/css/style.css" />
+
+	<script src = "assets/js/jquery-3.5.1.min.js"></script>
+	<script src = "assets/js/bootstrap.min.js"></script>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript"  src=" https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript"  src=" https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
