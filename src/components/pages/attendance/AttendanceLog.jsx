f12e2d7c56cb3f2afafd63a35be10f7b3aeb76ff
John Harvey Agrabio <agrabioharvey@gmail.com>
2022-11-22 18:47:09 +0800

    update

--- src/components/pages/attendance/AttendanceLog.jsx
+++ src/components/pages/attendance/AttendanceLog.jsx
@@ -4,8 +4,8 @@ const AttendancePage = () => {
     return (
         <div className='main-container'>
             <div className="attendance-container">
-         <h1>AttendancePage</h1>
-         <iframe src="http://localhost/api/employee-system/attendance.php" className="frame" scrolling='y'></iframe>
+         <h1>Attendance</h1>
+         <iframe src="http://localhost/api/employee-system/attendance.php" className="att_frame" scrolling='y'></iframe>
         
         </div>
         </div>
